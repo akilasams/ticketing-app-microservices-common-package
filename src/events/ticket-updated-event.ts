674da5de8492758b4704d4a9bad9beaab1da8266
Akila Samarawickrama <akilaishere@gmail.com>
2024-04-23 21:50:19 +0530

    Updates

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -1,7 +1,7 @@
 import { Subjects } from "./subjects";
 
 export interface TicketUpdatedEvent {
-  subjects: Subjects.TicketUpdated;
+  subject: Subjects.TicketUpdated;
   data: {
     id: string;
     title: string;
